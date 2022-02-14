264c2b04439c14c50b854199392b7e5c6acbbd98
searce-anubhav <anubhav.chakraborty@searce.com>
2022-02-14 11:49:00 +0530

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head />
+
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
